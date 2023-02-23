7a9eac943c9486ac93f82cc42a9aef01e6a0a474
roman <dlcourcerccf@gmail.com>
2023-02-23 13:40:58 +0300

    last drain

--- 1.5/good_string.py
+++ 1.5/good_string.py
@@ -1,15 +1,20 @@
 with open('input.txt', 'r') as r:
     lines = r.readlines()
-    print('lines :', lines)
-
-print(lines) 
 
 lines = [int(l) for l in lines]
 
-letters = {}    
-for i,l in enumerate(lines[1:]):
-    letters[i] = l
+letters_range = int(lines[0])
+letters = lines[1:]
 
-print(letters)
+ans = 0
+for i in range(len(letters)-1):
+
+    if letters[i] <= letters[i+1]:
+        ans += letters[i]
+    elif letters[i] > letters[i+1]:
+         ans += letters[i+1]
+
+with open('output.txt', 'w') as w:
+    w.write(str(ans))
 
 
