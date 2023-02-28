624411859230cd6861280d6722e0cb71326fa021
roman <dlcourcerccf@gmail.com>
2023-02-28 09:18:53 +0300

    last

--- sprint/21_ones/21.py
+++ sprint/21_ones/21.py
@@ -13,17 +13,17 @@ def ones(N):
     for i in range(1,N):
         _s = set()
         for e in s:
-            if len(e)-1 < 3:
+            if len(e) == 1:
                 _s.add(e+'0')
                 _s.add(e+'1')
             else:
                 _s.add(e+'0')
                 e_1 = e+'1'
-                if len(e_1[:-3]) !=  e_1[:-3].count('1'):
+                if len(e_1[-3:]) !=  e_1[-3:].count('1'):
                     _s.add(e_1)
         s = _s
 
-    print(s)
+    # print(s)
     return len(s)
 
 N  = int(open('input.txt', 'r').readline())
