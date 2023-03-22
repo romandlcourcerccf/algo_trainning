25162c7f9aba96071b35930358a05ad9920e6723
roman <dlcourcerccf@gmail.com>
2023-03-22 20:59:54 +0300

    417

--- leetcode/695._Max_Areaof_Island.py
+++ leetcode/695._Max_Areaof_Island.py
@@ -7,32 +7,36 @@ class Solution:
         cols = len(grid[0])
         rows = len(grid)
 
+        def _print():
+            for row in grid:
+                print(row)
+
         def dfs(col, row):
             nonlocal area
             area +=1
+            grid[row][col] = 2
 
             if col-1>= 0 and grid[row][col-1] == 1:
-                grid[row][col-1] = 2
                 dfs(col-1, row)
             
             if row-1>=0 and grid[row-1][col] == 1:
-                grid[row-1][col] = 2
                 dfs(col, row-1)
 
             if row+1 <= rows-1 and grid[row+1][col] == 1:
-                grid[row+1][col] = 2
                 dfs(col, row+1)
 
             if col+1 <= cols-1 and grid[row][col+1] == 1:
-                grid[row][col+1] = 2
                 dfs(col+1, row)
 
+        # _print()
+        # print('\n')
         
         for col in range(cols):
             for row in range(rows):
                 if grid[row][col] == 1:
                     area = 0
                     dfs(col, row)
-                max_area = max(max_area, area)
+                    max_area = max(max_area, area)
 
-        return max_area
+        # _print()
+        return max_area if max_area != float('-inf') else 0
