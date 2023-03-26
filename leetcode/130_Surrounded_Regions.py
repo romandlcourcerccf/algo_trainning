14b97417663e2316f09725e2476743b36fb85b82
roman <dlcourcerccf@gmail.com>
2023-03-26 16:23:06 +0300

    417

---
+++ leetcode/130_Surrounded_Regions.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols = len(board), len(board[0])
+
+        def dfs(row, col):
+            nonlocal is_isolated
+            nonlocal visited
+
+            if (row, col) in visited:
+                return 
+            
+            if row == 0 or row == rows-1 or col == 0 or row == cols-1:
+                is_isolated = False
+
+            visited.add((row, col))
+
+            if row+1 <= rows-1 and board[row+1][col] == 'O':
+                dfs(row, col)
+            
+            if row-1 >= 0 and board[row-1][col] == 'O':
+                dfs(row, col)
+
+            if col+1 <= cols-1 and board[row][col+1] == 'O':
+                dfs(row, col+1)
+            
+            if col-1 >= 0 and board[row][col-1] == 'O':
+                dfs(row, col-1)
+
+        def _print():
+            for r in range(rows):
+                print(board[r])
+
+        _print()   
+        print('\n')   
+        _visited = set()
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 'O' and (row,col) not in _visited:
+                    visited = set()
+                    is_isolated = True
+                    dfs(row, col)
+                    if is_isolated:
+                        for p in visited:
+                           board[p[0]][p[1]] = 'X' 
+                    _visited.update(visited)
+        _print()
+
