4a799286b1dadf55e99221cf15b1acf3e7b28258
roman <dlcourcerccf@gmail.com>
2023-04-10 23:01:44 +0300

    994

--- leetcode/994_Rotting_Oranges.py
+++ leetcode/994_Rotting_Oranges.py
@@ -3,47 +3,32 @@ class Solution:
         
         rows, cols = len(grid), len(grid[0])
 
-        def get_touched(v):
-            row, col = v[0], v[1] 
-            res = []
+        border = []
+        watched = set()
+        tics = 0
+        def infect():
+            nonlocal border
+            while len(border) > 0:
+                nonlocal tics
+                tics +=1
+                _border = []
+                for v in border:
+                    row, col = v[0], v[1]
+                
+                    if row+1 <= rows-1 and grid[row+1][col] == 1:
+                        grid[row+1][col] = 2
+                        _border.append((row+1, col))
+                    if row-1 >= 0 and grid[row-1][col] == 1:
+                        grid[row-1][col] = 2
+                        _border.append((row-1, col))  
+                    if col-1 >= 0 and grid[row][col-1] == 1:
+                        grid[row][col-1] = 2
+                        _border.append((row, col-1))
+                    if col+1 <= cols-1 and grid[row][col+1] == 1:
+                        grid[row][col+1] = 2
+                        _border.append((row, col+1))   
+                border = _border
+        border.append((0,0))
+        infect()
 
-            
-        def get_neighbors(v):
-           
-            row, col = v[0], v[1] 
-            res = []
-            if row-1>=0 and grid[row-1][col] == 1:
-                res.append((row-1, col))
-            if col-1>=0 and grid[row][col-1] == 1:
-                res.append((row, col-1))
-            if col+1<=cols-1 and grid[row][col+1] == 1:
-                res.append((row, col+1))
-            if row+1<=rows-1 and grid[row+1][col] == 1:
-                res.append((row+1, col))
-            return res
-    
-        def _print():
-            for r in grid:
-                print(r)
-
-        def bfs(l):
-            print('l :',l)
-            _l = set()
-            for v in l[list(l.keys())[-1]]:
-                grid[v[0]][v[1]] = 2
-                neighbors = get_neighbors(v)
-                _l.update(set(neighbors))
-               
-            if len(_l) == 0:
-                return
-
-            l[list(l.keys())[-1] + 1] = _l
-            bfs(l)
-
-        touched = set()
-        l = {0:{(0,0)}}
-        _print()
-        bfs(l)
-
-
-        return list(l.keys())[-1]
+        return tics-1
