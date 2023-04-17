75ab20c32d9d5ff5df1f9991501a2696b1204f8e
roman <dlcourcerccf@gmail.com>
2023-04-17 09:44:09 +0300

    207

---
+++ basic/1.py
@@ -0,0 +1,5 @@
+li = [[]] *  10
+
+li = [ [] for i in range(10)]
+li[0].append(10)
+print(li)
--- leetcode/207_Course Schedule.py
+++ leetcode/207_Course Schedule.py
@@ -3,17 +3,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
+        graph = [[] for _ in range(numCourses)]
 
-        def build_graph(prerequisites):
-            pass
-            
-        def find_circle(c):
-            pass
+        for p in prerequisites:
+            graph[p[0]].append(p[1])
 
-        for c in range(numCourses):
-            visited = set()
-            if find_circle(c):
-                return False
+        # print(graph)
 
-           
-        return True
+        black = set()
+        gray = set()
+        is_cycle = False
+
+        def dfs(node):
+            gray.add(node)
+            for v in graph[node]:
+                if v in gray:
+                    nonlocal is_cycle
+                    is_cycle = True
+                    return 
+                if v not in black:
+                    dfs(v)
+            gray.remove(node)
+            black.add(node)
+        
+                
+        for v in range(len(graph)):
+            if v not in black:
+                dfs(v)
+
+        # print('is_cycle :',is_cycle)
+
+        return not is_cycle
