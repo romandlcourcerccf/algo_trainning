a5dfa2884a3710f51c4131ceb04c9e7a66dbac2c
roman <dlcourcerccf@gmail.com>
2023-06-27 23:29:58 +0300

    215

---
+++ leetcode/215._Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,15 @@
+from heapq import(
+            heappush, 
+            heappop)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        h = []
+        for n in nums:
+            heappush(h, -n)
+        
+        for i in range(k):
+            e = heappop(h)
+
+        return -e
