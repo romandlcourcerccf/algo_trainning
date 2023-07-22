641e12e86dec62751b1ec33d6a41d5b2f9aca176
roman <dlcourcerccf@gmail.com>
2023-07-22 12:30:37 +0300

    last

--- leetcode/128._Longest_Consecutive_Sequence.py
+++ leetcode/128._Longest_Consecutive_Sequence.py
@@ -8,7 +8,9 @@ class Solution:
             if n-1 not in num_set:
                 cons_len = 0
                 while n + cons_len in num_set:
+                    num_set.remove(n + cons_len)
                     cons_len +=1
+                    
                 max_len = max(cons_len, max_len)
 
         return max_len
