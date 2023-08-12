30fde891d9b8091926c2a3e79733c8ca9e1bc9a2
roman <dlcourcerccf@gmail.com>
2023-08-12 18:32:19 +0300

    last

--- leetcode/111.py
+++ leetcode/111.py
@@ -4,3 +4,13 @@ arr.append(10)
 arr.append(20)
 
 print(arr[-1])
+
+s = set()
+
+if not s:
+    print('not s')
+
+s.add('11')
+
+if s:
+    print('s')
---
+++ leetcode/572. Subtree_of_Another_Tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+       
+        self.same  = True
+        self.hits = 0
+
+        def the_same(l, r):
+            if not l and not r:
+                return 
+
+            if (not l and r) or (l and not r) or (l.val != r.val):
+                self.same = False
+                return 
+            
+            the_same(l.left, r.left)
+            the_same(l.right, r.right)
+
+        def dfs(root):
+
+            if not root:
+                 return
+            
+            if root.val == subRoot.val:
+                self.same = True
+                the_same(root, subRoot)
+                self.hits += int(self.same)
+                
+            dfs(root.left)
+            dfs(root.right)
+
+        dfs(root)
+
+        return self.hits > 0
