6ecb4d65e16b42455373ce7bbde751e5ead8db05
Roman <roman@nastenka.darnet.ru>
2024-07-03 09:03:56 +0300

    last

---
+++ leetcode/1984. Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int: 
+        nums.sort()
+
+        min_diff = float('inf')
+
+        p1, p2 = 0, k-1
+
+        while p2 <= len(nums)-1:
+            min_diff = min(min_diff, nums[p2]-nums[p1])
+            p1+=1
+            p2+=1
+
+        return min_diff
