29e23b134d873d3c05b5ff50f685b286c801279e
Roman <dlcource_rccf@gmail.com>
2025-11-14 16:08:53 +0300

    last

--- yandex_back_prep/leetcode/238. Product of Array Except Self.py
+++ yandex_back_prep/leetcode/238. Product of Array Except Self.py
@@ -1,28 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        lr = [1] * len(nums)
-        ll = [1] * len(nums)
         res = [0] * len(nums)
 
-        for i in range(len(nums)):
-            if i == 0:
-                lr[i] = nums[i]
-            else:
-                lr[i] = nums[i] * lr[i-1]
-
-        for i in range(len(nums)-1, -1, -1):
-            if i == len(nums)-1:
-                ll[i] = nums[i]
-            else:
-                ll[i] = nums[i]*ll[i+1]
+        l_mult = [1] * len(nums)
+        r_mult = [1] * len(nums)
 
         for i in range(len(nums)):
             if i == 0:
-                res[i] = ll[i+1]
-            elif i == len(nums)-1:
-                res[i] = lr[i-1]
+                l_mult[i] = nums[i]
             else:
-                res[i] = lr[i-1] * ll[i+1]
-        
+                l_mult[i] = l_mult[i - 1] * nums[i]
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                r_mult[i] = nums[i]
+            else:
+                r_mult[i] = r_mult[i + 1] * nums[i]
+
+        for i in range(len(nums)):
+            if i == 0:
+                res[i] = r_mult[i + 1]
+            elif i == len(nums) - 1:
+                res[i] = l_mult[i - 1]
+            else:
+                res[i] = l_mult[i - 1] * r_mult[i + 1]
+
         return res
