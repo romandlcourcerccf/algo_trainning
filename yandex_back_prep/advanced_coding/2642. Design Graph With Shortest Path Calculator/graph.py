7dba3f0ab3ff3aaddc65d4e73f407ca714919b17
Roman <dlcource_rccf@gmail.com>
2025-12-15 16:43:42 +0300

    lasr

---
+++ yandex_back_prep/advanced_coding/2642. Design Graph With Shortest Path Calculator/graph.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from collections import defaultdict
+
+class Graph:
+    def __init__(self, n: int, edges: List[List[int]]):
+        self._edges = defaultdict(list)
+        for edge in edges:
+            self._edges[edge[0]].append(edge[1:])
+        
+    def addEdge(self, edge: List[int]):
+        self._edges.append(edge)
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        
+        
+
--- yandex_back_prep/leetcode/938. Range Sum of BST.py
+++ yandex_back_prep/leetcode/938. Range Sum of BST.py
@@ -12,19 +12,69 @@ class Solution(object):
         :type high: int
         :rtype: int
         """
-    
+
         self.acc = 0
 
         def dfs(root):
             if not root:
-                return 
-            
+                return
+
             if low <= root.val <= high:
                 self.acc += root.val
-            
+
             dfs(root.left)
             dfs(root.right)
 
         dfs(root)
 
         return self.acc
+
+    # Definition for a binary tree node.
+
+
+# Iterartion
+
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.range_sum = 0
+
+        # def dfs(root):
+        #     if not root:
+        #         return
+
+        #     if low <= root.val <= high:
+        #         self.range_sum += root.val
+
+        #     dfs(root.left)
+        #     dfs(root.right)
+
+        # dfs(root)
+
+        def dfs(root):
+            stack = [root]
+
+            cur = stack.pop()
+            while cur:
+                if low <= cur.val <= high:
+                    self.range_sum += cur.val
+
+                if cur.left:
+                    stack.append(cur.left)
+
+                if cur.right:
+                    stack.append(cur.right)
+
+                if stack:
+                    cur = stack.pop()
+                else:
+                    cur = None
+
+        dfs(root)
+
+        return self.range_sum
