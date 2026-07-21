df7fc72faa2e3ab96ec6557500ea8d29a4efc46a
Roman <dlcource_rccf@gmail.com>
2026-07-21 09:33:05 +0300

    228 350

--- yandex_back_prep_python/leetcode/228. Summary Ranges.py
+++ yandex_back_prep_python/leetcode/228. Summary Ranges.py
@@ -18,3 +18,30 @@ class Solution:
                 partial_sum.append(nums[i])
 
         return res
+    
+    class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        
+        res = []
+        i = 0
+        while i < len(nums):
+            _res = []
+            while i < len(nums)-1 and nums[i]+1 == nums[i+1]:
+                _res.append(nums[i])
+                i+=1
+            _res.append(nums[i])
+    
+            if len(_res) > 1:
+                res.append(f'{_res[0]}->{_res[-1]}')
+            else:
+                res.append(f'{_res[0]}')
+
+            i+=1
+
+        return res
+
+
+        
+
+
+
--- yandex_back_prep_python/leetcode/350. Intersection of Two Arrays II.py
+++ yandex_back_prep_python/leetcode/350. Intersection of Two Arrays II.py
@@ -13,3 +13,25 @@ class Solution:
 
         return res
 
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        result = []
+
+        nums1.sort()
+        nums2.sort()
+
+        p1, p2 = 0, 0
+
+        while p1 <= len(nums1) - 1 and p2 <= len(nums2) - 1:
+            if nums1[p1] == nums2[p2]:
+                result.append(nums1[p1])
+                p1 += 1
+                p2 += 1
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1
+
+        return result
