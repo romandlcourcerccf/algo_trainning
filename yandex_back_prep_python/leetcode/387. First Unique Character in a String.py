7bfa3aa63e595ed0a74dff058281035b734bbb64
Roman <dlcource_rccf@gmail.com>
2026-07-22 09:10:45 +0300

    387

--- yandex_back_prep_python/leetcode/387. First Unique Character in a String.py
+++ yandex_back_prep_python/leetcode/387. First Unique Character in a String.py
@@ -1,17 +1,28 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
+
         h = {}
 
         for c in s:
             if c not in h:
                 h[c] = 1
             else:
-                h[c] +=1
-        
+                h[c] += 1
+
         for i, c in enumerate(s):
             if h[c] == 1:
                 return i
 
         return -1
-        
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+
+        h = Counter(s)
+
+        for i, c in enumerate(s):
+            if h[c] == 1:
+                return i
+
+        return -1
